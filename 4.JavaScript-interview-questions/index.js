91785ac0148e5a5ebc620797a2c2b262f4cb2146
iamshivamgaur <shivam24august@gmail.com>
2025-03-10 13:11:13 +0530

    pushing js interveiw questions

---
+++ 4.JavaScript-interview-questions/index.js
@@ -0,0 +1,35 @@
+//^1.  What is javascript and where is it commonaly used?
+/*
+  Javascript is a high level programming language that is widly used
+  to create a interactive effect within web browser.
+  ---- A High level programming language is a human understandable 
+  language. it is used to developing both web and mobile applications
+*/
+
+//^2. What is template literals in Javascript?
+/*
+  When we combine $ sign {}brackets and backticks (``). It helps to embed
+  variable and expression direcly to the string to makin the code 
+  more readable and flexible
+
+  example:-
+  let a = 10;
+  let b = 20;
+  console.log(`Sum of ${a} and ${b} is ${a + b}`);
+*/
+
+//^3. What is hositing ? Give an example.
+
+greet();
+
+function greet() {
+  console.log("Hii there 👋");
+}
+
+/*
+ so in above we accessing the funtion before declaring in other langauge
+ like python so, it will throw an error.
+ --- But in JS it will execute and print the code. So this phenomena is 
+ know as hoisting
+*/
+
